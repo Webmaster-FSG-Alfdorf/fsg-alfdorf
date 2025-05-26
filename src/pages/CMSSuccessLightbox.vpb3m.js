c4c3f928b652579a3dd1e92174e8619e85ca613b
Oliver Hoffmann <oliverhoffmann32@gmail.com>
2025-05-26 06:46:30 +0200

    bump

--- src/pages/CMSSuccessLightbox.vpb3m.js
+++ src/pages/CMSSuccessLightbox.vpb3m.js
@@ -3,11 +3,15 @@ import wixWindowFrontend from "wix-window-frontend";
 $w.onReady(function () {
     const data = wixWindowFrontend.lightbox.getContext();
     if (data && data.msg) $w('#textSuccess').text = data.msg;
-    if (data.confirmations) $w("#buttonSendMail").show();
-    if (data.confirmations) $w("#buttonEditMail").show();
+    
     if (data.details) $w("#buttonDetails").show();
     $w("#buttonDetails").onClick(() => {
         $w("#buttonDetails").hide();
         $w('#textSuccess').text += "\n" + data.details;
+    });
+    
+    if (data.confirmations) $w("#buttonSendMail").show();
+    $w("#buttonSendMail").onClick(() => {
+
     });
 });
