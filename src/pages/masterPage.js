f8d8681eda5f5051d7662d5bf7158c57f8e31eb7
Oliver Hoffmann <oliverhoffmann32@gmail.com>
2026-02-26 09:46:55 +0100

    bump

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -10,23 +10,23 @@ $w.onReady(function () {
 
 function update() {
     currentMember.getRoles().then((roles) => {
-        toggleMenu(roles, "276cacd9-b43e-4e4e-8e3f-92192eb8eba7", "guests-management", "Gästeverwaltung", "guests-management");
-        toggleMenu(roles, "231ed231-93cf-45c1-9cbe-d99e7e45a27e", "events/edit", "Events bearbeiten", "events/edit");
-        toggleMenu(roles, "9cf0085c-a914-46de-a6b1-29aa4b86a76e", "food/edit", "Speisekarte  bearbeiten", "food/edit");
+        toggleMenu(roles, "276cacd9-b43e-4e4e-8e3f-92192eb8eba7", "guestReservations", "Gästeverwaltung", "guests-management");
+        toggleMenu(roles, "231ed231-93cf-45c1-9cbe-d99e7e45a27e", "events", "Events bearbeiten", "events/edit");
+        toggleMenu(roles, "9cf0085c-a914-46de-a6b1-29aa4b86a76e", "Speisen", "Speisekarte bearbeiten", "food/edit");
     });
 }
 
-function toggleMenu(roles, roleID, menuID, label, url) {
+function toggleMenu(roles, roleID, dataSet, label, url) {
     const showMenu = roles.some((role) => role._id == roleID);
     const menu = $w("#expandableMenu");
-    const mi = menu.menuItems;
-    const menuShown = mi.some(item => item.id == menuID);
+    const menuShown = menu.menuItems.some(item => item.id == url);
     if (showMenu && !menuShown) {
-        console.log(`masterPage - going to show ${menuID} menu item`);
-        wixData.query("guestReservations").descending("_updatedDate").limit(1).find().then((res) => {
-            console.log(`masterPage - going to show ${menuID} menu item for ${res.items.map(i => i._id)}`);
+        console.log(`masterPage - going to show ${url} menu item`);
+        wixData.query(dataSet).descending("_updatedDate").limit(1).find().then((res) => {
+            console.log(`masterPage - going to show ${url} menu item for ${res.items.map(i => i._id)}`);
+            const mi = menu.menuItems;
             mi.push({
-                id: menuID,
+                id: url,
                 link: res.items.length > 0 ? `/${url}/${res.items[0]._id}` : `/${url}`, //FIXME test with length == 0
                 label: label,
                 target: "_self",
@@ -37,7 +37,7 @@ function toggleMenu(roles, roleID, menuID, label, url) {
         });
     }
     if (!showMenu && menuShown) {
-        console.log(`masterPage - going to hide ${menuID} menu item`);
-        menu.menuItems = mi.filter(item => item.id != menuID);
+        console.log(`masterPage - going to hide ${url} menu item`);
+        menu.menuItems = menu.menuItems.filter(item => item.id != url);
     }
 }
