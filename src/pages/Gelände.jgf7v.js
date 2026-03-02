c55d11d0bee42536b57374dc104b5ee24172e303
Oliver Hoffmann <oliverhoffmann32@gmail.com>
2026-03-02 15:25:37 +0100

    bump

--- src/pages/Gelände.jgf7v.js
+++ src/pages/Gelände.jgf7v.js
@@ -5,20 +5,18 @@ $w.onReady(async function () {
         wixWindow.openLightbox("InteractiveMap");
     });
 
-    //  const { items } = await wixData.query("mapAreas").find();
+    const { items } = await wixData.query("mapAreas").find();
 
-    // 2. Daten für Google Maps aufbereiten
-    const formattedAreas = []; //items.map(item => ({
-    /*
-    name: item.title,
-    descr: item.description,
-    category: item.category, // sport, infra, places
-    url: item.url || "",
-    // Falls dein Pfad im CMS als Text/String gespeichert ist:
-    path: typeof item.path === 'string' ? JSON.parse(item.path) : item.path,
-    images: item.imageGallery ? item.imageGallery.map(img => img.src.split('/').pop()) : []
-}));
-*/
+    const formattedAreas = items.map(item => ({
+        name: item.title,
+        descr: item.description,
+        category: item.category,
+        placeNumber: item.placeNumber,
+        url: item.url,
+        path: item.path ? JSON.parse(item.path) : [],
+        images: item.images ? item.images.map(img => img.src.split('/').pop()) : []
+    }));
+
     $w("#htmlMap").onMessage((event) => {
         if (event.data === "ready") {
             $w("#htmlMap").postMessage(formattedAreas);
