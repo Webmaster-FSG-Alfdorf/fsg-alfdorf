537e3f4bf46b9be479f58b5ff70217131664e768
Oliver Hoffmann <oliverhoffmann32@gmail.com>
2026-03-04 08:29:36 +0100

    bump

--- src/pages/Gelände.jgf7v.js
+++ src/pages/Gelände.jgf7v.js
@@ -8,7 +8,7 @@ $w.onReady(async function () {
 
     $w("#htmlMap").onMessage(async (event) => {
         if (event.data === "ready") {
-            const { items } = await wixData.query("mapAreas").find();
+            const { items } = await wixData.query("mapAreas").limit(1000).find();
             $w("#htmlMap").postMessage(items);
         }
     });
--- src/pages/InteractiveMap.yls9q.js
+++ src/pages/InteractiveMap.yls9q.js
@@ -3,7 +3,7 @@ import wixData from 'wix-data';
 $w.onReady(async function () {
 	$w("#htmlMap").onMessage(async (event) => {
 		if (event.data === "ready") {
-			const { items } = await wixData.query("mapAreas").find();
+			const { items } = await wixData.query("mapAreas").limit(1000).find();
 			$w("#htmlMap").postMessage(items);
 		}
 	});
