bb33bbf4239ac6cda2ad566da6b3dafe9ca1f576
Oliver Hoffmann <oliverhoffmann32@gmail.com>
2026-03-13 09:48:19 +0100

    x

--- src/pages/masterPage.js
+++ src/pages/masterPage.js
@@ -10,7 +10,7 @@ $w.onReady(function () {
 
 function update() {
     currentMember.getRoles().then((roles) => {
-        toggleMenu(roles, "276cacd9-b43e-4e4e-8e3f-92192eb8eba7", "guestReservations", "Gästeverwaltung", "guests-management");
+        toggleMenu(roles, "276cacd9-b43e-4e4e-8e3f-92192eb8eba7", null, "Gästeverwaltung", "guests-management");
         toggleMenu(roles, "231ed231-93cf-45c1-9cbe-d99e7e45a27e", "events", "Events bearbeiten", "events/edit");
         toggleMenu(roles, "9cf0085c-a914-46de-a6b1-29aa4b86a76e", "foods", "Speisekarte bearbeiten", "food/edit");
     });
@@ -22,7 +22,7 @@ function toggleMenu(roles, roleID, dataSet, label, url) {
     const menuShown = menu.menuItems.some(item => item.id == url);
     if (showMenu && !menuShown) {
         console.log(`masterPage - going to show ${url} menu item`);
-        wixData.query(dataSet).descending("_updatedDate").limit(1).find().then((res) => {
+        if (dataSet) wixData.query(dataSet).descending("_updatedDate").limit(1).find().then((res) => {
             console.log(`masterPage - going to show ${url} menu item for ${res.items.map(i => i._id)}`);
             const mi = menu.menuItems;
             mi.push({
@@ -34,7 +34,18 @@ function toggleMenu(roles, roleID, dataSet, label, url) {
                 "menuItems": []
             });
             menu.menuItems = mi;
-        });
+        }); else {
+            const mi = menu.menuItems;
+            mi.push({
+                id: url,
+                link: `/${url}`,
+                label: label,
+                target: "_self",
+                "selected": false,
+                "menuItems": []
+            });
+            menu.menuItems = mi;
+        }
     }
     if (!showMenu && menuShown) {
         console.log(`masterPage - going to hide ${url} menu item`);
